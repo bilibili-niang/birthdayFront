4fba571f3b80c95b7dc629eb8fab155cb69f6672
张嘉凯 <killicestone@126.com>
2023-10-16 00:49:28 +0800

    feat:修改部分配置

---
+++ src/pages/autonomic/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="autonomic">
+    自律
+  </view>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/pages/discovery/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="discovery">
+    发现
+  </view>
+
+</template>
+
+<style scoped lang="less">
+
+</style>
